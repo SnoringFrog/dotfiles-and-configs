e211a41ea325448d0b169fbc8e1a9b0a612a810e
Ethan Piekarski <Ethan.Piekarski@genworth.com>
2015-03-18 15:23:07 -0400

    Add fish shell config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if test (date +%w) -ne 5
+	switch $SHELL
+		case '*fish*'
+			echo -e "Default shell is fish and it isn't Friday.\nSwitching back to bash."
+			chsh -s /bin/bash
+			# Open new tab, requires https://gist.github.com/bobthecow/757788
+			tab #Need to check if this is installed
+			exit
+	end
+end
