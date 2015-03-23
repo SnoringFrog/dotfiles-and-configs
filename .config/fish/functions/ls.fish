dfada86db381db228bd0f16ecb05656138ccb969
Ethan Piekarski <Ethan.Piekarski@genworth.com>
2015-03-23 12:11:40 -0400

    ls accounts for OSX and GNU core utils

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,18 @@
-function ls
-	command ls -AFG $argv
+# ls alias: human readable, show dotfiles, filetype indicators, color,
+# directories first (if possible)
+
+if [ (uname -s) = "Darwin" ] # If on OSX 
+	if hash gls ^/dev/null # If GNU Coreutils installed
+		function ls
+			command gls -hAF --color --group-directories-first $argv
+		end
+	else
+		function ls
+			command ls -hAFG $argv
+		end
+	end
+else 
+	function ls
+		command ls -hAF --color --group-directories-first $argv
+	end
 end
